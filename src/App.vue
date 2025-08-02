ed5d069ad0b076121948822f10c197cda2cd7ef1
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-08-02 12:23:45 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <h1>Xin chào từ Vue 3!</h1>
+    <p>Dự án mẫu để thử deploy</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
